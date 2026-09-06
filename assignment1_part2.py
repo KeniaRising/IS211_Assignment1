6e75d4bb52386f25249abecee8ea44860ff39f5c
Kenia Ventura <kv24289741@SPS-IS211126910.sps.cuny.edu>
2026-09-05 20:42:33 -0400

    Complete assignment 1 part 2

--- assignment1_part2.py
+++ assignment1_part2.py
@@ -1,12 +1,22 @@
 
 class Book:
+    author = ""
+    title = ""
+
     def __init__(self, author, title):
-        pass
-    
+        self.author = author
+        self.title = title
+
     def display(self):
-        pass
+        print(self.title + ", written by " + self.author)
+
+book1 = Book("J. K. Rowling", "Harry Potter and the Goblet of Fire")
+book2 = Book("Walter Scott", "Ivanhoe: A Romance")
+
+book1.display()
+book2.display()
+
+   
 
 
-if __name__ == "__main__":
-    a = Book("Thorpe", "Beat the Dealer")
-    print("This is a assignment 1 part 2")
+
